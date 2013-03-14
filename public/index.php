89a0d0b32968563363faeae5f2f933b1b7f8560e
Equan Pr <eq@junwatu.com>
2013-03-14 17:10:03 +0700

    app startup

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *  Simple Shopping Chart
+ *
+ *
+ *  Equan Pr.
+ *  www.junwatu.com
+ *  2013
+ */
+
+use Symfony\Component\HttpFoundation\Request;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+require '../vendor/autoload.php';
+
+$log = new Logger('Watushop');
+$log->pushHandler(new StreamHandler('../log/watushop.log'));
+
+$request = Request::createFromGlobals();
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Shopping Cart</title>
+</head>
+<body>
+<h1>Simple Shopping Cart</h1>
+</body>
+</html>
