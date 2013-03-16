f66002a6ee9e3c405ebb9f18da28485ce496c695
Equan Pr <eq@junwatu.com>
2013-03-16 19:21:09 +0700

    data sample.

---
+++ public/data.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: Equan Pr.
+ * Date: 3/16/13
+ * Time: 6:25 PM
+ */
+
+
+// Sample data
+$items = array(
+    array('id' => '1', 'desc' => 'Learning PHP Design Patterns', 'price' => 15.81),
+    array('id' => '2', 'desc' => 'Effortless E-Commerce PHP and MySQL', 'price' => 23.09),
+    array('id' => '3', 'desc' => 'Secret of the JavaScript Ninja', 'price' => 22.31),
+    array('id' => '4', 'desc' => 'Simply JavaScript (SitePoint)', 'price' => 39.95));
