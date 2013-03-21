2119fd7c73b26a3b88393daace14ba8a7b922b79
Equan Pr <eq@junwatu.com>
2013-03-22 06:57:22 +0700

    change data sample.

--- public/data.php
+++ public/data.php
@@ -9,6 +9,7 @@
 // Sample data
 $items = array(
     array('id' => '1', 'desc' => 'Learning PHP Design Patterns', 'price' => 15.81),
-    array('id' => '2', 'desc' => 'Effortless E-Commerce PHP and MySQL', 'price' => 23.09),
+    array('id' => '2', 'desc' => 'PHP & MySQL Novice To Ninja, 5th Edition', 'price' => 22.31),
     array('id' => '3', 'desc' => 'Secret of the JavaScript Ninja', 'price' => 22.31),
-    array('id' => '4', 'desc' => 'Simply JavaScript (SitePoint)', 'price' => 39.95));
+    array('id' => '4', 'desc' => 'Simply JavaScript (SitePoint)', 'price' => 39.95),
+);
