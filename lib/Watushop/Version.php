de349aa52ac514680128b8ad76a1df407619acb6
Equan Pr <eq@junwatu.com>
2013-03-22 08:43:30 +0700

    custom lib

---
+++ lib/Watushop/Version.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: Equan Pr.
+ * Date: 3/14/13
+ * Time: 3:53 PM
+ */
+
+namespace Watushop;
+
+class Version {
+
+    static $APP_VERSION = "1.0-alpha";
+
+    public function __construct(){
+
+    }
+}
