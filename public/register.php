75620f71ca5326d8a8a17b3d083ac582859114f7
Equan Pr <eq@junwatu.com>
2013-03-30 06:45:32 +0700

    init

---
+++ public/register.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * User: Equan Pr.
+ * Date: 3/30/13
+ * Time: 6:38 AM
+ */
+require '../vendor/autoload.php';
+
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+
+$loader = new UniversalClassLoader();
+$loader->useIncludePath(true);
+
+/**
+ * Register custom library
+ */
+$loader->registerNamespaces(array(
+    'Watushop' => __DIR__ . '/../lib'
+));
+
+$loader->register();
+
