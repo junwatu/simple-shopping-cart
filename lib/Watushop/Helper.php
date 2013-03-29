91c70966a54b57332617a4f42241e98d85c0ba4a
Equan Pr <eq@junwatu.com>
2013-03-30 06:45:08 +0700

    helper html class

---
+++ lib/Watushop/Helper.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * User: Equan Pr.
+ * Date: 3/30/13
+ * Time: 6:34 AM
+ */
+
+namespace Watushop;
+
+class Helper {
+
+    private function html($text)
+    {
+        return htmlspecialchars($text, ENT_QUOTES, 'UTF-8');
+    }
+
+    public function htmlout($text)
+    {
+        echo $this->html($text);
+    }
+}
