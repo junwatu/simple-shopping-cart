21612a3e1e11997e738552ad213d85da08d034e4
Equan Pr <eq@junwatu.com>
2013-03-30 06:47:09 +0700

    remove one data

--- public/data.php
+++ public/data.php
@@ -10,6 +10,5 @@
 $items = array(
     array('id' => '1', 'desc' => 'Learning PHP Design Patterns', 'price' => 15.81, 'cover' => 'images/learn_php_des_pat_cover.jpg'),
     array('id' => '2', 'desc' => 'PHP & MySQL Novice To Ninja, 5th Edition', 'price' => 22.31, 'cover' => 'images/phpmysql5_cover.jpg'),
-    array('id' => '3', 'desc' => 'Secret of the JavaScript Ninja', 'price' => 22.31, 'cover'=> 'images/javascript_ninja_cover.jpg'),
-    array('id' => '4', 'desc' => 'Simply JavaScript (SitePoint)', 'price' => 39.95, 'cover'=>'images/simply_javascript_cover.jpg')
+    array('id' => '3', 'desc' => 'Secret of the JavaScript Ninja', 'price' => 22.31, 'cover'=> 'images/javascript_ninja_cover.jpg')
 );
