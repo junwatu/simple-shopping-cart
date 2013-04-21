e002fc6deb9dfb79ebc8c01d3e643cebb86affdc
Equan Pr <eq@junwatu.com>
2013-04-21 08:07:47 +0700

    add more data

--- public/data.php
+++ public/data.php
@@ -10,5 +10,11 @@
 $items = array(
     array('id' => '1', 'desc' => 'Learning PHP Design Patterns', 'price' => 15.81, 'cover' => 'images/learn_php_des_pat_cover.jpg'),
     array('id' => '2', 'desc' => 'PHP & MySQL Novice To Ninja, 5th Edition', 'price' => 22.31, 'cover' => 'images/phpmysql5_cover.jpg'),
-    array('id' => '3', 'desc' => 'Secret of the JavaScript Ninja', 'price' => 22.31, 'cover'=> 'images/javascript_ninja_cover.jpg')
+    array('id' => '3', 'desc' => 'Secret of the JavaScript Ninja', 'price' => 22.31, 'cover'=> 'images/javascript_ninja_cover.jpg'),
+    array('id' => '4', 'desc' => 'Programming PHP, 5th Edition', 'price' => 22.31, 'cover'=> 'images/programming-php.jpg'),
+    array('id' => '5', 'desc' => 'Think Like A Programmer', 'price' => 38.45, 'cover'=> 'images/think-like-aprogrammer.jpg'),
+    array('id' => '6', 'desc' => 'The Healthy Programmer Get Fit, Feel Better, and Keep Coding', 'price' => 33.00, 'cover'=> 'images/the-healthy-programmer.jpg'),
+    array('id' => '7', 'desc' => 'Interface Oriented Design', 'price' => 29.95, 'cover'=> 'images/interface-design.jpg'),
+    array('id' => '8', 'desc' => 'Interactive Data Visualization for the Web An Introduction to Designing with D3', 'price' => 32.99, 'cover'=> 'images/interactive-data-vis.jpg')
+
 );
